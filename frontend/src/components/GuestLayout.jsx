07d5d337cb7e9e0ef467a5d8437e49954aa1314d
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-10-16 16:16:28 +0800

    create protected routes

--- frontend/src/components/GuestLayout.jsx
+++ frontend/src/components/GuestLayout.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider.jsx";
 
 function GuestLayout() {
+  const { token } = useStateContext();
+
+  if (token) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <p>Guest layout</p>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
